ae5112ac0535f33e4d22311b96ad5a08485d3a24
Anand <venkatanandn@gmail.com>
2026-02-03 13:05:00 +0530

    two mahcine configs added

---
+++ ChangeSystemClock.ps1
@@ -0,0 +1,17 @@
+﻿Configuration ChangeSystemClock {
+    Import-DscResource -ModuleName 'PSDscResources'
+
+    Node localhost
+    {
+        Registry "ShowSecondsInSystemClock"
+        {
+            Ensure = "Present"
+            Key = "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+            ValueName = "ShowSecondsInSystemClock"
+            ValueData = "1"
+            ValueType = "DWORD"
+            Force = $true
+        }
+    }
+}
+ChangeSystemClock
---
+++ testMachineConfig.ps1
@@ -0,0 +1,34 @@
+﻿<# 
+Reason for non-compliance
+No related resources match the effect details in the policy definition. (Error code: NoComplianceReport)
+#>
+
+Configuration testMachineConfig {
+    Import-DscResource -ModuleName 'PSDscResources'
+
+    Node localhost
+    {
+        # Registries to Disable TLS 1.1
+        Registry "DisableTLS 1.1_Server"
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server"
+            ValueName = "DisabledByDefault"
+            ValueData = "1"
+            ValueType = "DWORD"
+            Force = $true
+        }
+
+        Registry "DisableTLS 1.1_ServerDisabled"
+        {
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server"
+            ValueName = "Enabled"
+            ValueData = "0"
+            ValueType = "DWORD"
+            Force = $true
+        }
+    }
+
+}
+testMachineConfig
